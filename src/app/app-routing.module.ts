ddd5de4ce21865982692ee7bbb042b60bb33265e
Lucas C Correa <lucascorrea@uzotto.com>
2021-07-14 00:40:02 -0300

    added default route redirect to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
     path: 'chat-bot',
