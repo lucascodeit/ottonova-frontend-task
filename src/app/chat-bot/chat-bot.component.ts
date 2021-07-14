b553fb71c24d6b9e42699c327c2248264a2bb7e3
Lucas C Correa <lucascorrea@uzotto.com>
2021-07-14 00:29:05 -0300

    added feature to scroll bottom when size scroll increase

--- src/app/chat-bot/chat-bot.component.ts
+++ src/app/chat-bot/chat-bot.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { ChatService } from './../../services/chat-service';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat-bot',
   templateUrl: './chat-bot.component.html',
-  styleUrls: ['./chat-bot.component.scss']
+  styleUrls: ['./chat-bot.component.scss'],
 })
-export class ChatBotComponent implements OnInit {
+export class ChatBotComponent implements AfterViewInit {
+  @ViewChild('wrapperChat') wrapperChat?: ElementRef<HTMLElement>;
 
-  constructor() { }
+  constructor(private chatService: ChatService) {}
 
-  ngOnInit(): void {
+  private handleScrollToBottom() {
+    this.chatService.chatMessagesSubject.pipe(delay(100)).subscribe(() => {
+      if (!this.wrapperChat) return;
+      const divScroll = this.wrapperChat.nativeElement
+        .getElementsByClassName('mat-tab-body-content')
+        .item(0);
+      if (divScroll) divScroll.scrollTop = divScroll.scrollHeight;
+    });
   }
 
+  ngAfterViewInit(): void {
+    this.handleScrollToBottom();
+  }
 }
