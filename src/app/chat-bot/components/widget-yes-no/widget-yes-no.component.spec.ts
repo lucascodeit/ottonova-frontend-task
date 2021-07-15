bbc3a911e932478c9c402783cbdb3b62b4b35047
Lucas C Correa <lucascorrea@uzotto.com>
2021-07-14 22:04:54 -0300

    added tests for widget-yes-no component

--- src/app/chat-bot/components/widget-yes-no/widget-yes-no.component.spec.ts
+++ src/app/chat-bot/components/widget-yes-no/widget-yes-no.component.spec.ts
@@ -10,16 +10,19 @@ describe('WidgetYesNoComponent', () => {
   let component: WidgetYesNoComponent;
   let fixture: ComponentFixture<WidgetYesNoComponent>;
 
+  let mockRouter = {
+    navigateByUrl: jasmine.createSpy('navigateByUrl'),
+  };
+  let mockChatService = {
+    clearChat: jasmine.createSpy('clearChat'),
+  };
   beforeEach(async () => {
-    let mockRouter = {
-      navigate: jasmine.createSpy('navigate'),
-    };
     await TestBed.configureTestingModule({
       declarations: [WidgetYesNoComponent],
       providers: [
         WidgetYesNoComponent,
         { provide: Router, useValue: mockRouter },
-        { provide: ChatService },
+        { provide: ChatService, useValue: mockChatService },
         { provide: OttonovaService, useClass: OttonovaServiceMock },
       ],
     }).compileComponents();
@@ -28,10 +31,39 @@ describe('WidgetYesNoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WidgetYesNoComponent);
     component = fixture.componentInstance;
+    component.data = ['Yes', 'No'];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should clear Chat and navigate to login if click yes', () => {
+    fixture.detectChanges();
+    const divElement: HTMLElement = fixture.nativeElement;
+    const button = divElement.getElementsByTagName('button').item(0);
+    button?.click();
+    fixture.detectChanges();
+    expect(mockChatService.clearChat).toHaveBeenCalledOnceWith();
+    expect(mockRouter.navigateByUrl).toHaveBeenCalledOnceWith('/login');
+  });
+
+  it('should show start question ', () => {
+    const divElement: HTMLElement = fixture.nativeElement;
+    const pElement = divElement.getElementsByTagName('p').item(0);
+    expect(pElement?.textContent).toBe(
+      'Would you like to close this conversations?'
+    );
+  });
+
+  it('should show message enjoy our chat bot if click no', () => {
+    fixture.detectChanges();
+    const divElement: HTMLElement = fixture.nativeElement;
+    const button = divElement.getElementsByTagName('button').item(1);
+    button?.click();
+    fixture.detectChanges();
+    const pElement = divElement.getElementsByTagName('p').item(0);
+    expect(pElement?.textContent).toBe('Ok, enjoy with our ottonova bot!');
   });
 });
